f678c47be1c1ad0b5128987bc8a926d496d1517c
Ian-Geraldi <iangc49@gmail.com>
2023-06-17 19:08:25 -0300

    .

--- scripts/move_to_old.py
+++ scripts/move_to_old.py
@@ -7,8 +7,6 @@ def move_to_old():
     src_dir = "input"
     dst_dir = "input_old"
 
-    if not os.path.exists(src_dir):
-        raise Exception("Source directory does not exist")
     # create the destination directory if it doesn't exist
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
@@ -20,6 +18,17 @@ def move_to_old():
     for file_name in files:
         # construct full file path
         src_file_path = os.path.join(src_dir, file_name)
+
+        # If file with the same name exists in the destination directory,
+        # append a number in brackets to the filename until a unique name is found
+        if os.path.isfile(os.path.join(dst_dir, file_name)):
+            # split the filename into name and extension
+            base_name, ext = os.path.splitext(file_name)
+            counter = 1
+            while os.path.isfile(os.path.join(dst_dir, f"{base_name}({counter}){ext}")):
+                counter += 1
+            file_name = f"{base_name}({counter}){ext}"
+
         dst_file_path = os.path.join(dst_dir, file_name)
 
         # move the file to the destination directory
